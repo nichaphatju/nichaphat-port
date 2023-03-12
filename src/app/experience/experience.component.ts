8e55bd6d0aa937bf8b67560d366d1226eac20e4c
Nichaphat J <nichaphat.jump@gmail.com>
2023-03-12 21:53:34 +1100

    Update experience

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -15,21 +15,31 @@ export class ExperienceComponent implements OnInit {
         name: 'ASG Group',
         website: 'https://www.asggroup.com.au/'
       }, 
-      detail: '•	Working with team and business to migrate current features and data from three existing salesforce instances into one instance.<br/> •	Implement new features to create recurring tasks, and activities, sending email page to remind users to send reports via email using lightning aura component and lightning web component.'
+      detail: `•	Working with team and business to migrate current features and data from three existing salesforce instances into one instance.
+      <br/> •	Implement new features to create recurring tasks, and activities, sending email page to remind users to send reports via email using lightning aura component and lightning web component.`
     },
     {time: 'Jul 2021 – Jul 2022', position:'Salesforce Developer', 
       company: {
-        name: 'Beryl8',
-        website: 'https://www.beryl8.com/en/home'
+        name: 'ExxonMobil Thailand (belong to Pantheon Corperation Thailand)',
+        website: ''
       }, 
-      detail: 'test'
+      detail: `•	Be a part of an agile team in ExxonMobil, both develop and maintain salesforce customized applications, permission, security settings, custom objects, and workflow.
+      <br/>•	Develop customized solutions within the Salesforce platform to support online order and sales capabilities
+      <br/>•	Collaborate with various business users, system engineers, and architects to ensure the design and development meet the business needs.
+      `
     },
-    {time: 'Jan 2021 – Jul 2021', position:'Technical Consultant', 
+    {time: 'Apr 2018 – Jul 2021', position:'Senior Salesforce Developer - Technical Consultant', 
       company: {
         name: 'Beryl8',
         website: 'https://www.beryl8.com/en/home'
       }, 
-      detail: 'test'
+      detail: `•	Estimate working time, Design and build custom pages, apex batch, and apex triggers, customize apex code for customers according to user’s requirement (usually focus on sales and service process) using APEX, HTML, JavaScript, Lightning, Aura component, and Visualforce Page. 
+      My responsibilities include config basic standard salesforces features e.g., Flow, Process builder, and Assignment rule in financial and oil businesses.
+      <br/>•	Provide REST Service API using APEX programming language to integrate with the customer system.
+      <br/>•	Designed and Implemented modules, for example, a user management process, loan calculation process, and lead assignment process for an enterprise organization. 
+      <br/>•	Do unit tests. Create test classes to cover at least 75% of the project code and finish the deployment which mostly uses a gearset.
+      <br/>•	Create technical documents e.g., integration specifications. Consult and discuss solutions to solve issues with the IT team of customers in the meetings.
+      `
     },
     {time: 'Apr 2018 – Dec 2020', position:'Senior Salesforce Developer', 
       company: {
